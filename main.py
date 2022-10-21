7efb9f919cfee2f8f8dad49567cf69f8485df329
Jonas Silva <jonaslsa@uio.no>
2022-10-22 00:05:02 +0200

    Better types

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import os
 import io
 from pprint import pprint
 from enum import Enum, auto
-from typing import Any, List, Tuple
+from typing import Any, List, Tuple, Union
 import struct
 from jvm_opcode import Opcode
 
@@ -84,12 +84,12 @@ method_access_flags : List[Tuple[str, int]] = [
 def parse_flags(value: int, flags: List[Tuple[str, int]]) -> List[str]:
     return [name for (name, mask) in flags if (value & mask) != 0]
 
-def parse_u1(f : io.BytesIO):
+def parse_u1(f : Union[io.BytesIO, io.BufferedReader]):
     return struct.unpack('>B', f.read(1))[0]
-def parse_i1(f : io.BytesIO) -> int: return int.from_bytes(f.read(1), 'big')
-def parse_i2(f : io.BytesIO) -> int: return int.from_bytes(f.read(2), 'big')
-def parse_i4(f : io.BytesIO) -> int: return int.from_bytes(f.read(4), 'big')
-def parse_f4(f : io.BytesIO) -> int: return struct.unpack('>f', f.read(4))[0]
+def parse_i1(f : Union[io.BytesIO, io.BufferedReader]) -> int: return int.from_bytes(f.read(1), 'big')
+def parse_i2(f : Union[io.BytesIO, io.BufferedReader]) -> int: return int.from_bytes(f.read(2), 'big')
+def parse_i4(f : Union[io.BytesIO, io.BufferedReader]) -> int: return int.from_bytes(f.read(4), 'big')
+def parse_f4(f : Union[io.BytesIO, io.BufferedReader]) -> int: return struct.unpack('>f', f.read(4))[0]
 
 def u16_to_i16(v: int) -> int:
     if v & 0x8000:
